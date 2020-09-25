33978d3d73e75c44eaba8555e5e9cb3fa4464f6d
comradecoastal <endermedia12@gmail.com>
2020-09-25 18:10:03 +0300

    Удаление мусора

--- konstantin.py
+++ konstantin.py
@@ -1,5 +1,5 @@
 print("введите 3 целых числа через пробел")
 
-a, b, c = map(abs,map(int,input().split()))
+a, b, c = map(abs, map(int, input().split()))
 
-print("kon"*a+"stan"*b+"tin"*c)
+print("kon" * a + "stan" * b + "tin" * c)
